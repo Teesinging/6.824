064c01afffa49d5b8cc4fb576d886b98785c780b
Teesinging <1016463975@qq.com>
2024-07-04 14:49:20 +0800

    lab3 ALL PASS!

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -4,11 +4,15 @@ import (
 	"crypto/rand"
 	"lab1/src/labrpc"
 	"math/big"
+	"time"
 )
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	clientId    int64 // client的唯一标识符
+	knownLeader int   // 已知的leader，请求到非leader的server后的reply中获取
+	commandNum  int   // 标志client为每个command分配的序列号到哪了（采用自增，从1开始）
 }
 
 func nrand() int64 {
@@ -22,6 +26,10 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.clientId = nrand()
+	ck.knownLeader = -1
+	ck.commandNum = 1
+
 	return ck
 }
 
@@ -38,7 +46,55 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	return ""
+
+	args := GetArgs{
+		Key:      key,
+		ClientId: ck.clientId,
+		CmdNum:   ck.commandNum,
+	}
+
+	i := 0 // 从server 0开始发送请求
+	serversNum := len(ck.servers)
+	for ; ; i = (i + 1) % serversNum { // 循环依次向每一个server请求直至找到leader
+
+		if ck.knownLeader != -1 { // 如果找到了leader
+			i = ck.knownLeader // 下次直接向leader发起请求
+		} else {
+			time.Sleep(time.Millisecond * 5) // 为了不短时间内再次请求非leader的server
+		}
+
+		reply := GetReply{}
+
+		DPrintf("Client[%d] request [%d] for Get(key:%v)......\n", ck.clientId, i, args.Key)
+		ok := ck.servers[i].Call("KVServer.Get", &args, &reply)
+
+		if !ok {
+			DPrintf("Client[%d] Get request failed!\n", ck.clientId)
+			ck.knownLeader = -1 // 没收到回复则下次向其他kvserver发送
+			continue
+		} else { // 成功收到rpc回复
+			switch reply.Err {
+			case OK:
+				ck.commandNum++
+				ck.knownLeader = i
+				DPrintf("Client[%d] request(Seq:%d) for Get(key:%v) [successfully]!\n", ck.clientId, args.CmdNum, args.Key)
+				return reply.Value
+			case ErrWrongLeader:
+				ck.knownLeader = -1
+				DPrintf("Client[%d] request(Seq:%d) for Get(key:%v) but [WrongLeader]!\n", ck.clientId, args.CmdNum, args.Key)
+				continue
+			case ErrNoKey:
+				ck.commandNum++
+				ck.knownLeader = i
+				DPrintf("Client[%d] request(Seq:%d) for Get(key:%v) but [NoKey]!\n", ck.clientId, args.CmdNum, args.Key)
+				return ""
+			case ErrTimeout:
+				ck.knownLeader = -1
+				DPrintf("Client[%d] request(Seq:%d) for Get(key:%v) but [Timeout]!\n", ck.clientId, args.CmdNum, args.Key)
+				continue
+			}
+		}
+	}
 }
 
 // shared by Put and Append.
@@ -49,8 +105,60 @@ func (ck *Clerk) Get(key string) string {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
+// Put和Append操作都会调它，相当于集成（因为KVServer的这两个操作是用一个函数 实现）
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+
+	args := PutAppendArgs{
+		Key:      key,
+		Value:    value,
+		Op:       op,
+		ClientId: ck.clientId,
+		CmdNum:   ck.commandNum,
+	}
+
+	i := 0
+	serversNum := len(ck.servers)
+	for ; ; i = (i + 1) % serversNum {
+
+		if ck.knownLeader != -1 {
+			i = ck.knownLeader
+		} else {
+			time.Sleep(time.Millisecond * 5) // 为了不短时间内再次请求非leader的server
+		}
+
+		reply := PutAppendReply{}
+
+		DPrintf("Client[%d] request [%d] for PutAppend(key:%v, value:%v)......\n", ck.clientId, i, args.Key, args.Value)
+		ok := ck.servers[i].Call("KVServer.PutAppend", &args, &reply)
+
+		if !ok {
+			DPrintf("Client[%d] PutAppend request failed!\n", ck.clientId)
+			ck.knownLeader = -1 // 没收到回复则下次向其他kvserver发送
+			continue
+		} else {
+			switch reply.Err {
+			case OK:
+				ck.commandNum++
+				ck.knownLeader = i
+				DPrintf("Client[%d] request(Seq:%d) for PutAppend [successfully]!\n", ck.clientId, args.CmdNum)
+				return
+			case ErrWrongLeader:
+				ck.knownLeader = -1
+				DPrintf("Client[%d] request(Seq:%d) for PutAppend but [WrongLeader]!\n", ck.clientId, args.CmdNum)
+				continue
+			case ErrNoKey:
+				ck.commandNum++
+				ck.knownLeader = i
+				DPrintf("Client[%d] request(Seq:%d) for Append but [NoKey]!\n", ck.clientId, args.CmdNum)
+				return
+			case ErrTimeout:
+				ck.knownLeader = -1
+				DPrintf("Client[%d] request(Seq:%d) for PutAppend but [Timeout]!\n", ck.clientId, args.CmdNum)
+				continue
+			}
+		}
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
